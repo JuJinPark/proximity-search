33809555015590f42e82cb248cfbdc908e536770
JuJinPark <wnwls1216@gmail.com>
2025-04-22 18:21:37 +0900

    feat: first commit with structure

---
+++ src/main/java/com/example/proximity_search/ProximitySearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.proximity_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProximitySearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProximitySearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proximity_search/ProximitySearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proximity_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProximitySearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
